5a3936dfbb53715ce22e0ee4400e6a6dccc35550
cole-thompson <colethompson@utexas.edu>
2017-09-25 19:06:15 -0500

    implemented refreshPos, cleaned up other methods

--- Oscillator.cpp
+++ Oscillator.cpp
@@ -6,33 +6,36 @@
 #ifndef OSCILLATOR
   #include "Oscillator.h"
 
-
 Oscillator::Oscillator() {
-  isStopped = false;
+  //init helper variables
+  isStopped = true;
+  servoAttached = false;
   t_lastRefresh = 0;
-}
+  ph = 0;
 
-
-//SERVO SETUP
-void Oscillator::attach(int pin) {
-  servo.detach();
-  servo.attach(pin);
-}
-void Oscillator::detach() {
-  if (servo.attatched()) {
-    servo.detach();
-  }
+  //default sinusoid values
+  samplePeriod = 30;
+  setAmp(40);
+  setOff(0);
+  setPh0(0);
+  setPeriod(2500);
+  setRev(false);
+  setTrim(0);       //can use setTrim() to calibrate if needed
 }
 
 
 //SINUSOID FUNCTIONS
 //set servo pos based on sinusoid
 void Oscillator::refreshPos() {
-  //TODO
-  //sample a sin function
-  //check current servo position
-  //update variables
-
+  if (servoAttached && checkRefreshTime()) {
+    if (!isStopped) {
+        //if the refresh time is complete and the motor is not stopped
+        //calculate the current pos in the sinusoid
+        pos = rev * round(amp * sin(ph + ph0) + off);
+        servo.write(pos + trim);
+    }
+    ph += phInc;    //increment the phase
+  }
 }
 //check if refresh time increment has passed
 bool Oscillator::checkRefreshTime() {
@@ -46,6 +49,20 @@ bool Oscillator::checkRefreshTime() {
 }
 
 
+//SERVO SETUP
+void Oscillator::attach(int pin) {
+  servo.detach();
+  servo.attach(pin);
+  servoAttached = true;
+}
+void Oscillator::detach() {
+  if (servo.attatched()) {
+    servo.detach();
+    servoAttached = false;
+  }
+}
+
+
 //SINUSOID PARAMETERS
 //set Amplitude (degrees)
 void Oscillator::setAmp(unsigned int a) {amp = a;}
@@ -53,23 +70,32 @@ void Oscillator::setAmp(unsigned int a) {amp = a;}
 void Oscillator::setOff(unsigned int o) { off = o;}
 //set Initial Phase (radians)
 void Oscillator::setPh0(double p0) {ph0 = p0;}
-//set Position (degrees)
-void Oscillator::setPos(int p) {pos = p;}
+//set Period (ms)
+void Oscillator::setPeriod(unsigned int t) {
+  period = t;
+  int n = period / samplePeriod;  //n = number of samples
+  phInc = 2 * PI / n;
+}
+//Set Sinusoid Reverse on/off (default off) (sin reverse not servo reverse)
+void Oscillator::setRev(bool r) {
+  if (r) {rev = -1}
+  else {rev = 1}
+}
 
 
 //CONTROL
-void Oscillator::stop() {
-  isStopped = true;
+void Oscillator::stop() {isStopped = true;}
+void Oscillator::start() {isStopped = false;}
+//set Position (degrees)
+void Oscillator::setPos(int p) {
+  pos = p;
+  servo.write(pos + trim);
 }
-void Oscillator::start() {
-  isStopped = false;
+//set Current Phase to 0
+void resetPh() {
+  ph = 0;
 }
-
 
 //CALIBRATION (if we need it)
-void Oscillator::setTrim(int t) {
-  trim = t;
-}
-int Oscillator::getTrim() {
-  return trim;
-}
+void Oscillator::setTrim(int t) {trim = t;}
+int Oscillator::getTrim() {return trim;}
--- Oscillator.h
+++ Oscillator.h
@@ -1,14 +1,25 @@
 /* Oscillator.h
  * UT Austin RAS Demobots
- * Oscillate motors in sinusoidal motion
+ * Oscillate servos in sinusoidal motion
  * Basically just a wrapper class for the Arduino library servo class
  */
 
 //this prevents errors if Oscillator.h is included in multiple files
 #ifndef OSCILLATOR
   #define OSCILLATOR
+  #define PI 3.14159265358979323846
 
 #include <Servo.h>
+
+//TODO create OscillatorTest, update these steps if needed
+
+/* HOW TO USE OSCILLATOR CLASS
+ * 1) create Oscillator object
+ * 2) attach it to a pin
+ * 3) set desired sinusoid parameters, calibrate and set trim if needed
+ * 4) run refreshPos() on the main loop
+ * 5) enable oscillation with start()
+ */
 
 
 class Oscillator {
@@ -25,15 +36,18 @@ public:
   void setAmp(unsigned int a);    //set Amplitude (degrees)
   void setOff(unsigned int o);    //set Offset (degrees)
   void setPh0(double p0);         //set Initial Phase (radians)
-  void setPos(int p);             //set Position (degrees)
+  void setPeriod(unsigned int t); //set Period (ms)
+  void setRev(bool r);            //Set Reverse on/off (default off)
 
   //control
   void stop();
   void start();
+  void setPos(int p);             //set Position (degrees)
+  void resetPh();                 //set Current Phase to 0
 
-  //calibration (if we need it)
-  void setTrim(int t);       //set Trim
-  int getTrim();             //get Trim
+  //calibration (if we need it, built into refreshPos, just call setTrim)
+  void setTrim(int t);       //set Trim (degrees)
+  int getTrim();             //get Trim (degrees)
 
 private:
   //sinusoid functions
@@ -43,12 +57,14 @@ private:
 
   //Arduino Servo object
   Servo servo;
+  bool servoAttached;         //true when a servo is attached
 
   //sinusoid parameters
   unsigned int amp;     //Amplitude (degrees)
   unsigned int off;     //Offset (degrees)
   double ph0;           //Initial Phase (radians)
   unsigned int period;  //Period (ms)
+  int rev;              //Reverse Sinusoid multiplier (1 = no rev, -1 = rev)
 
   //servo status variables
   bool isStopped;
