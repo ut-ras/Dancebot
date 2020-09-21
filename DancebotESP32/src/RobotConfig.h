a178821df3e22403c60539aff87e8f48b9b870c0
Matthew Yu <matthewjkyu@gmail.com>
2020-09-21 02:33:14 -0500

    #5 shrunk down the configuration requirements for a Demobot Dancebot.

--- DancebotESP32/src/RobotConfig.h
+++ DancebotESP32/src/RobotConfig.h
@@ -1,47 +1,17 @@
 /**
- * Author: Matthew Yu
- * Last Modified: 05/22/20
- * Project: Demobots General
  * File: RobotConfig.h
- * Description: A config file to be used by WebController.h and DemobotAPI.h.
+ * Author: Matthew Yu
+ * Last Modified: 09/21/20
+ * Project: Demobots General
  * Organization: UT IEEE RAS
+ * Description: A configuration file to be used by WebController.h and
+ * DemobotAPI.h to define the Demobot internal representation.
  * NOTE: Modify the MODIFIABLE INFO section for your robot.
  */
-#ifndef ROBOTCONFIG_H
-#define ROBOTCONFIG_H
-
-#define MAX_ROBOTS 20       // I seriously doubt that we'll ever have more than 5 robots on a single webserver (i.e. Dancebots)
-#define CRED_LOG_LEN 5      // arbitrarily decided max credentials defined at any time
-#define RETRY_WAIT 200      // 200 ms
-#define UPDATE_WAIT 150     // 150 ms
-#include <IPAddress.h>
-
-extern const int ROBOT_ID;
-
-extern IPAddress gateway;
-extern IPAddress subnet;
-/**
- * Represents a single possible network that can be connected to or established.
- * Has a SSID and WPA2/PSK password associated with it.
- */
-typedef struct Credential {
-    char* SSID;
-    char* PASSWORD;
-} Credential;
-extern Credential CredentialsLog[];
-extern const int DEFAULT_NETWORK_ID;
-/**
- * Represents a single Demobot type and has an ID and IPAddress it belongs to.
- */
-typedef struct DemobotNetwork {
-    int ID;
-    String name;
-    IPAddress defaultIP;
-} DemobotNetwork;
-extern DemobotNetwork Robots[];
-
+#pragma once
 // --------------------------- MODIFIABLE INFO ---------------
-// FOR DANCEBOT
+
+// Dancebot Definition
 enum DancebotStates {
     Reset,
     Walk,
@@ -50,18 +20,41 @@ enum DancebotStates {
     Ankles,
     Demo1,
     Demo2,
-    NumStates
+    NumStates,
 };
 
-extern char* dancebotStates[];
-extern int numConnectedRobots;
+enum RobotExpression {
+    Off,
+    Happy,
+    Sad,
+    Neutral,
+};
+
+enum RobotEyeColor {
+    White,
+    Red,
+    Green,
+    Blue,
+};
 
 typedef struct Dancebot {
+    char* robotType;
     int robotID;
-    int robotState;
-    int robotEyeColor;
-    int robotExpression;
+    double soc; // state of charge
+    DancebotStates robotState;
+    RobotExpression robotExpression;
+    RobotEyeColor robotEyeColor;
 } Dancebot;
-extern Dancebot connectedRobots[MAX_ROBOTS];
 
-#endif
+// Individual Dancebot Definition
+char name[9] = "Dancebot";
+Dancebot dancebot1 {
+    name,
+    0,
+    100.0,
+    Reset,
+    Off,
+    White,
+};
+
+
