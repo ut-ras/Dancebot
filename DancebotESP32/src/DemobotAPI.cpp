47988c0ff740a4a9ce14a255e72ef9de3230c136
Matthew Yu <matthewjkyu@gmail.com>
2020-09-25 01:07:56 -0500

    added documentation and code framework for parsing messages and doing movements.

--- DancebotESP32/src/DemobotAPI.cpp
+++ DancebotESP32/src/DemobotAPI.cpp
@@ -35,43 +35,83 @@ void move() {
 
 /**
  * updateState updates the internal function of the DancingServos* bot.
- * @param String state - encoded version of the server expectation of the Dancebot state.
+ * @param state: String
+ *      encoded version of the server expectation of the Dancebot state.
+ *      We expect the following types of messages:
+ *          - "" (no or bad response)
+ *          - "[JOIN]"  (robot has joined the network)
+ *      - "[LEFT]"  (robot has left the network willingly and the server
+ *      acquiesced. Shutdown the robot.)
+ *      - "[UPDT] ROBOT_ID,MOVE,EYE_COLOR,EYE_EXPRESSION,SHUTDOWN"
+ *          - a [UPDT], update identifier, and the following CSV with no spaces:
+ *              - Robot ID: check to see if we got the right data for the robot
+ *              - Robot Movement: next state to change to
+ *              - Eye Color: robot eye color to set to
+ *              - Eye Expression: robot eye expression to set to
+ *              - Shutdown: supercedes Robot Movement. Calls the shutdown routine 
+ *                  if applicable. 
  */
 void updateState(String state) {
-    // Serial.println("Robot received new state: " + state);
-    
-    int moveIdx = state.indexOf(':');
-    int moveIdxEnd = state.indexOf(';', moveIdx+1);
-    String move = state.substring(moveIdx+1, moveIdxEnd);
-    // Serial.println(move);
+    // parse identifier
 
-    switch(move.toInt()) {
-        case Reset:
-            bot->position0();
-            bot->enableDanceRoutine(false);
-            break;
-        case Walk:
-            bot->walk(-1, 1500, false);
-            break;
-        case Hop:
-            bot->hop(25, -1);
-            break;
-        case Wiggle:
-            bot->wiggle(30, -1);
-            break;
-        case Ankles:
-            bot->themAnkles(-1);
-            break;
-        case Demo1:
-            bot->setDanceRoutine(0);
-            bot->enableDanceRoutine(true);
-            break;
-        case Demo2:
-            bot->setDanceRoutine(1);
-            bot->enableDanceRoutine(true);
-            break;
-        default:
-            Serial.println("Invalid state.");
+    if (state.equals("")) {
+        /* We received a bad response. Do nothing this iteration. */
+        return;
+    } else if (state.substring(0, 6).equals("[JOIN]")) {
+        /**
+         * Robot received an affirmative for a /robotJoin request.
+         * TODO: do we need to do anything here?
+         */
+        return;
+    } else if (state.substring(0, 6).equals("[LEFT]")) {
+        /**
+         * Robot received an affirmative for a /robotLeave request.
+         * Call the shut down routine.
+         */
+        shutdown();
+    } else if (state.substring(0, 6).equals("[UPDT]")) {
+        /**
+         * Robot received an affirmative for a /robotUpdate request.
+         * Parse further for more instructions.
+         */
+        // TODO: parse move from index 7 to delimiter ','
+        int move = 0;
+        switch(move) {
+            case Reset:
+                bot->position0();
+                bot->enableDanceRoutine(false);
+                break;
+            case Walk:
+                bot->walk(-1, 1500, false);
+                break;
+            case Hop:
+                bot->hop(25, -1);
+                break;
+            case Wiggle:
+                bot->wiggle(30, -1);
+                break;
+            case Ankles:
+                bot->themAnkles(-1);
+                break;
+            case Demo1:
+                bot->setDanceRoutine(0);
+                bot->enableDanceRoutine(true);
+                break;
+            case Demo2:
+                bot->setDanceRoutine(1);
+                bot->enableDanceRoutine(true);
+                break;
+            default:
+                Serial.println("Invalid state.");
+            // TODO: parse eye color to the next delimeter ','
+            // TODO: parse eye expression to the next delimeter ','
+            // TODO: parse shutdown to the end of the line.
+            // TODO: if shutdown, send a /robotLeave request to the server.
+        }
+
+    } else {
+        /** Some invalid format. Do nothing this iteration. */
+        return;
     }
 }
 
@@ -81,4 +121,8 @@ void updateState(String state) {
  */
 bool isOscillating() {
     return bot->isOscillating();
+}
+
+void shutdown() {
+    /* TODO: shutdown routine here calling the auto off driver. */
 }
