81dea44a6b15dec595be2aaa0d8f6804b0ee187a
Anthony Luu <anthonyluu191@gmail.com>
2024-03-22 17:40:23 -0500

    Routing rev 6, trace width increased, smd components added

---
+++ MainDancebotTest/src/PowerController.h
@@ -0,0 +1,31 @@
+/* PowerController.h
+ * UT Austin RAS Demobots
+ * 
+ *
+ *
+*/
+
+#ifndef DANCINGSERVOS
+#define DANCINGSERVOS
+
+class PowerController {
+public:
+    //when ESP32 turns on, latch the power to continue being on (refer to sch)
+    void powerOnSystem(void);
+
+    //sleeping functions
+    void sleepSystem(void);
+    void sleepSystem(int period);
+    void awakeSystem(void);
+
+    //set and get battery % between 0 - 100
+    int getBatteryPercentage(void);
+    void setBatteryPercentage(void);
+
+private:
+    //0 - 100 %
+    int batteryPercentage;
+
+};
+
+#endif
