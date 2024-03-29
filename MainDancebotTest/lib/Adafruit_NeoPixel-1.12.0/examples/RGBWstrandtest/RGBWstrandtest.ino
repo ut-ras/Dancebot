aff670947624465897d526626915f7157eed675e
gracewl15 <gracewl15051@gmail.com>
2024-03-29 17:21:21 -0500

    Added the Library and Example code onto DancingServos. Need to figure out the timer function and will test hopefully next week

---
+++ MainDancebotTest/lib/Adafruit_NeoPixel-1.12.0/examples/RGBWstrandtest/RGBWstrandtest.ino
@@ -0,0 +1,284 @@
+// NeoPixel test program showing use of the WHITE channel for RGBW
+// pixels only (won't look correct on regular RGB NeoPixel strips).
+
+#include <Adafruit_NeoPixel.h>
+#include <chrono>
+#ifdef __AVR__
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN1     25
+#define LED_PIN2     26
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(40, LED_PIN1);
+
+int mode   = 0; // Current animation effect
+int offset = 0;
+int color  = 0X00A4B3; // Starting color
+int prevTime; 
+
+void setup() {
+  pixels.begin();
+  pixels.setBrightness(40); // 1/3 brightness
+  prevTime; //Need to figure out millisecond counter here
+}
+
+void loop() {
+  uint8_t  i;
+  uint32_t t;
+
+  switch(mode) {
+    
+   case 0: //rainbow hold
+    rainbowHold(20);
+    //delay(500); make loop for delay
+    break;
+    
+   case 1: //rainbow cycle slow
+    rainbowCycleslow(20);
+    //delay(50);
+    break;
+       
+   case 2: //rainbow cycle fast 
+    rainbowCycle(5);
+    //delay(50);
+    break;
+  }
+
+  //t = millis(); figure out milisecond counter
+  if((t - prevTime) > 8000) {      // Every 8 seconds...
+    mode++;                        // Next mode
+    if(mode > 3) {                 // End of modes?
+      mode = 0;                    // Start modes over
+      color >>= 8;                 // Next color R->G->B
+      if(!color) color = 0xB300A4; // Reset color
+    }
+    for(i=0; i<32; i++) pixels.setPixelColor(i, 0);
+    prevTime = t;
+    
+  }
+
+  
+}
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    pixels.show();
+    //delay(wait);
+  }
+}
+
+void rainbowCycle(uint8_t wait) {
+  uint16_t r, j;
+
+  for(j=0; j<256*6; j++) { // 6 cycles of all colors on wheel
+    for(r=0; r< pixels.numPixels(); r++) {
+      pixels.setPixelColor(r, Wheel(((r * 256 / pixels.numPixels()) + j) & 255));
+    }
+    pixels.show();
+    //delay(wait);
+  }
+}
+void rainbowCycleslow(uint8_t wait) {
+  uint16_t r, j;
+
+  for(j=0; j<256*3; j++) { // 3 cycles of all colors on wheel
+    for(r=0; r< pixels.numPixels(); r++) {
+      pixels.setPixelColor(r, Wheel(((r * 256 / pixels.numPixels()) + j) & 255));
+    }
+    pixels.show();
+    //delay(wait);
+  }
+}
+void rainbowHold(uint8_t wait) {
+  uint16_t r, j;
+
+  for(j=0; j<256*1; j++) { // 3 cycles of all colors on wheel
+    for(r=0; r< pixels.numPixels(); r++) {
+      pixels.setPixelColor(r, Wheel(((r * 256 / pixels.numPixels()) + j) & 255));
+    }
+    pixels.show();
+    //delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(int8_t WheelPos) {
+  if(WheelPos < 85) {
+   return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+void setup() {
+  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
+  // Any other board, you can remove this part (but no harm leaving it):
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(BRIGHTNESS);
+}
+
+void loop() {
+  // Fill along the length of the strip in various colors...
+  colorWipe(strip.Color(255,   0,   0)     , 50); // Red
+  colorWipe(strip.Color(  0, 255,   0)     , 50); // Green
+  colorWipe(strip.Color(  0,   0, 255)     , 50); // Blue
+  colorWipe(strip.Color(  0,   0,   0, 255), 50); // True white (not RGB white)
+
+  whiteOverRainbow(75, 5);
+
+  pulseWhite(5);
+
+  rainbowFade2White(3, 3, 1);
+}
+
+// Fill strip pixels one after another with a color. Strip is NOT cleared
+// first; anything there will be covered pixel by pixel. Pass in color
+// (as a single 'packed' 32-bit value, which you can get by calling
+// strip.Color(red, green, blue) as shown in the loop() function above),
+// and a delay time (in milliseconds) between pixels.
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+  }
+}
+
+void whiteOverRainbow(int whiteSpeed, int whiteLength) {
+
+  if(whiteLength >= strip.numPixels()) whiteLength = strip.numPixels() - 1;
+
+  int      head          = whiteLength - 1;
+  int      tail          = 0;
+  int      loops         = 3;
+  int      loopNum       = 0;
+  uint32_t lastTime      = millis();
+  uint32_t firstPixelHue = 0;
+
+  for(;;) { // Repeat forever (or until a 'break' or 'return')
+    for(int i=0; i<strip.numPixels(); i++) {  // For each pixel in strip...
+      if(((i >= tail) && (i <= head)) ||      //  If between head & tail...
+         ((tail > head) && ((i >= tail) || (i <= head)))) {
+        strip.setPixelColor(i, strip.Color(0, 0, 0, 255)); // Set white
+      } else {                                             // else set rainbow
+        int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+        strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+      }
+    }
+
+    strip.show(); // Update strip with new contents
+    // There's no delay here, it just runs full-tilt until the timer and
+    // counter combination below runs out.
+
+    firstPixelHue += 40; // Advance just a little along the color wheel
+
+    if((millis() - lastTime) > whiteSpeed) { // Time to update head/tail?
+      if(++head >= strip.numPixels()) {      // Advance head, wrap around
+        head = 0;
+        if(++loopNum >= loops) return;
+      }
+      if(++tail >= strip.numPixels()) {      // Advance tail, wrap around
+        tail = 0;
+      }
+      lastTime = millis();                   // Save time of last movement
+    }
+  }
+}
+
+void pulseWhite(uint8_t wait) {
+  for(int j=0; j<256; j++) { // Ramp up from 0 to 255
+    // Fill entire strip with white at gamma-corrected brightness level 'j':
+    strip.fill(strip.Color(0, 0, 0, strip.gamma8(j)));
+    strip.show();
+    delay(wait);
+  }
+
+  for(int j=255; j>=0; j--) { // Ramp down from 255 to 0
+    strip.fill(strip.Color(0, 0, 0, strip.gamma8(j)));
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbowFade2White(int wait, int rainbowLoops, int whiteLoops) {
+  int fadeVal=0, fadeMax=100;
+
+  // Hue of first pixel runs 'rainbowLoops' complete loops through the color
+  // wheel. Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to rainbowLoops*65536, using steps of 256 so we
+  // advance around the wheel at a decent clip.
+  for(uint32_t firstPixelHue = 0; firstPixelHue < rainbowLoops*65536;
+    firstPixelHue += 256) {
+
+    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the strip
+      // (strip.numPixels() steps):
+      uint32_t pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+
+      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the three-argument variant, though the
+      // second value (saturation) is a constant 255.
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue, 255,
+        255 * fadeVal / fadeMax)));
+    }
+
+    strip.show();
+    delay(wait);
+
+    if(firstPixelHue < 65536) {                              // First loop,
+      if(fadeVal < fadeMax) fadeVal++;                       // fade in
+    } else if(firstPixelHue >= ((rainbowLoops-1) * 65536)) { // Last loop,
+      if(fadeVal > 0) fadeVal--;                             // fade out
+    } else {
+      fadeVal = fadeMax; // Interim loop, make sure fade is at max
+    }
+  }
+
+  for(int k=0; k<whiteLoops; k++) {
+    for(int j=0; j<256; j++) { // Ramp up 0 to 255
+      // Fill entire strip with white at gamma-corrected brightness level 'j':
+      strip.fill(strip.Color(0, 0, 0, strip.gamma8(j)));
+      strip.show();
+    }
+    delay(1000); // Pause 1 second
+    for(int j=255; j>=0; j--) { // Ramp down 255 to 0
+      strip.fill(strip.Color(0, 0, 0, strip.gamma8(j)));
+      strip.show();
+    }
+  }
+
+  delay(500); // Pause 1/2 second
+}
--- MainDancebotTest/src copy/DancingServos.cpp
+++ MainDancebotTest/src copy/DancingServos.cpp
@@ -8,6 +8,20 @@
 
 #include <Arduino.h>
 #include "DancingServos.h"
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define LED_PIN1     25
+#define LED_PIN2     26
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT  1
+
+// NeoPixel brightness, 0 (min) to 255 (max)
+#define BRIGHTNESS 50 // Set BRIGHTNESS to about 1/5 (max = 255)
 
 //SETUP FUNCTIONS
 DancingServos::DancingServos(int hL, int hR, int aL, int aR) {
@@ -91,6 +105,110 @@ void DancingServos::waitOscillation() {
 
 bool DancingServos::isOscillating() {
   return isOsc;
+}
+
+//LED Functions
+void setup() {
+  pixels.begin();
+  pixels.setBrightness(40); // 1/3 brightness
+  prevTime; //Need to figure out millisecond counter here
+}
+
+void loop() {
+  uint8_t  i;
+  uint32_t t;
+
+  switch(mode) {
+    
+   case 0: //rainbow hold
+    rainbowHold(20);
+    //delay(500); make loop for delay
+    break;
+    
+   case 1: //rainbow cycle slow
+    rainbowCycleslow(20);
+    //delay(50);
+    break;
+       
+   case 2: //rainbow cycle fast 
+    rainbowCycle(5);
+    //delay(50);
+    break;
+  }
+
+  //t = millis(); figure out milisecond counter
+  if((t - prevTime) > 8000) {      // Every 8 seconds...
+    mode++;                        // Next mode
+    if(mode > 3) {                 // End of modes?
+      mode = 0;                    // Start modes over
+      color >>= 8;                 // Next color R->G->B
+      if(!color) color = 0xB300A4; // Reset color
+    }
+    for(i=0; i<32; i++) pixels.setPixelColor(i, 0);
+    prevTime = t;
+    
+  }
+
+  
+}
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    pixels.show();
+    //delay(wait);
+  }
+}
+
+void rainbowCycle(uint8_t wait) {
+  uint16_t r, j;
+
+  for(j=0; j<256*6; j++) { // 6 cycles of all colors on wheel
+    for(r=0; r< pixels.numPixels(); r++) {
+      pixels.setPixelColor(r, Wheel(((r * 256 / pixels.numPixels()) + j) & 255));
+    }
+    pixels.show();
+    //delay(wait);
+  }
+}
+void rainbowCycleslow(uint8_t wait) {
+  uint16_t r, j;
+
+  for(j=0; j<256*3; j++) { // 3 cycles of all colors on wheel
+    for(r=0; r< pixels.numPixels(); r++) {
+      pixels.setPixelColor(r, Wheel(((r * 256 / pixels.numPixels()) + j) & 255));
+    }
+    pixels.show();
+    //delay(wait);
+  }
+}
+void rainbowHold(uint8_t wait) {
+  uint16_t r, j;
+
+  for(j=0; j<256*1; j++) { // 3 cycles of all colors on wheel
+    for(r=0; r< pixels.numPixels(); r++) {
+      pixels.setPixelColor(r, Wheel(((r * 256 / pixels.numPixels()) + j) & 255));
+    }
+    pixels.show();
+    //delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(int8_t WheelPos) {
+  if(WheelPos < 85) {
+   return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
 }
 
 
