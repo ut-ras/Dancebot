5417de7c3c46542a0699279e4c685082455d5863
Saahir Narang <90425039+saahirN@users.noreply.github.com>
2025-01-15 10:57:20 -0600

    Delete DemobotLegsESP32 directory

--- DemobotLegsESP32/WebController.h
+++
@@ -1,8 +0,0 @@
-#ifndef WEBCONTROLLER
-#define WEBCONTROLLER
-
-void setupWiFi(String mode, const char * _ssid, const char * _pass);
-void setupWebServer(DancingServos* _bot);
-void loopWebServer();
-
-#endif
