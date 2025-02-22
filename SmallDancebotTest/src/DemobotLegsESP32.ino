394ceaa61c0a46b47117aa96121c1dc04ec07dfa
Toby Nguyen <tobyanguyen1@gmail.com>
2025-02-21 21:56:46 -0600

    adjusted servo pins for pcb

--- SmallDancebotTest/src/DemobotLegsESP32.ino
+++ SmallDancebotTest/src/DemobotLegsESP32.ino
@@ -1,38 +1,4 @@
-//ESP32 PICO Kit dev board
-/* Current Pins
- * hip  L: 14
- * hip R: 13
- * ankle L: 12 
- * ankle R: 15
- */
-
-/*
- * Hardware
- * 
- * Servos
- *    "Ankles": 2 HiTec HS-625MG "Super Torque" Servos https://www.horizonhobby.com/pdf/HRC32625S-Spec_Sheet.pdf
- *      Operating Voltage: 4.8-6.0 V
- *      Current Draw: 8.8 mA idle, 400 mA running
- *    "Hips": 2 HiTec HS-322HD "Deluxe" Servos https://www.robotshop.com/media/files/pdf/hitec-hs-322hd-servo-specsheet.pdf
- *      Operating Voltage: 4.8-6.0 V
- *      Current Draw: 7.4 mA idle, 160 mA running no load, 700 mA running stalled
- *    Total Current Draw 4 Servos: ~1600mA when running
- *    Both have 900-2100 us pulse width (see Oscillator.cpp attach function)
- * Microcontroller
- *    ESP32 Pico Kit https://www.espressif.com/sites/default/files/documentation/esp32-pico-d4_datasheet_en.pdf
- *    Operating Voltage: There is a 5v pin
- *    Current Draw: 80mA average
- * Batteries
- *    Power Source: 6V 8000 mAH
- *    4 rechargeable AA (1.5V), 2000 mAH each
- *    8000 mAH battery using 1700mA lasts about 4.7 hours
- * Decoupling Capacitor
- *    100uF capacitor across the power source. I'm not sure if this is the best size, I haven't measured the noise yet.
- *    Decoupling capacitors help remove AC noise from a DC power source.
- *    The motors turning on/off and running can create noise in the power source; look at the differnce in current draw for idle vs running.
- *    The ESP32 will reset if the power supply is not stable. The Teensy didn't crash without the cap, but it doesn't hurt. 
- *    It would probably be better to use a 5v regulator.
- */
+//ESP32 devkitC
 
  
 #include <Arduino.h>
@@ -71,7 +37,7 @@ void setup() {
   // Serial.println("I started setting up!");
 
   //[hipL, hipR, ankleL, ankleR]
-  bot = new DancingServos(14, 13, 12, 15);
+  bot = new DancingServos(21, 23, 19, 22);
   calibrateTrims(bot);
   bot->position0();
 
@@ -100,17 +66,6 @@ void loop() {
 
   //check if ready to start next move in dance
   bot->loopDanceRoutines();
-
-  // handleDanceMove();
-
-  // for (pos = 0; pos <= 180; pos += 1) {
-  //   hat.write(pos);
-  //   delay(10);
-  // }
-  // for (pos = 180; pos >= 0; pos -= 1) {
-  //   hat.write(pos);
-  //   delay(10);
-  // }
 }
 
 
